4f694cfd582c3cd0d452d8affecfd4f6afdaa9ae
irenicaa <root@irenica.pro>
2022-08-10 00:08:57 +0300

    Fix the /campaigns/{campaign_id}/orders/{order_id}/status endpoint

--- yandex_seller/__main__.py
+++ yandex_seller/__main__.py
@@ -3,20 +3,27 @@ import os
 
 import dotenv
 
-from . import credentials, orders_delivery_labels, request_api
+from . import credentials, orders_status, request_api
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
     try:
+        data = orders_status.SetStatusRequest(
+            order = orders_status.OrderStatus(
+                status = 'PROCESSING',
+                substatus = 'READY_TO_SHIP',
+            )
+        )
+
         ya_credentials = credentials.Credentials(os.getenv('CLIENT_ID'), os.getenv('CLIENT_TOKEN'))
 
-        pdf = orders_delivery_labels.get_campaigns_orders(
+        status = orders_status.set_order_status(
             ya_credentials,
             os.getenv('COMPANY_NUMBER'),
             os.getenv('ORDER_NUMBER'),
+            data
         )
-        with open('test.pdf', 'wb') as f:
-            f.write(pdf)
+        print(status)
     except request_api.HTTPError as error:
         print('ERROR', error)
         print('ERROR response_data', error.response_data)
--- yandex_seller/orders_status.py
+++ yandex_seller/orders_status.py
@@ -31,54 +31,54 @@ class SetStatusRequest:
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDeliveryShipmentBoxItem:
-    id: int
-    count: int
+    id: Optional[int] = None
+    count: Optional[int] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDeliveryShipmentBox:
-    id: int
-    height: int
-    depth: int
-    width: int
-    weight: int
-    items: list[SetOrderStatusResponseResultOrderDeliveryShipmentBoxItem]
+    id: Optional[int] = None
+    height: Optional[int] = None
+    depth: Optional[int] = None
+    width: Optional[int] = None
+    weight: Optional[int] = None
+    items: Optional[list[SetOrderStatusResponseResultOrderDeliveryShipmentBoxItem]] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDeliveryShipment:
-    id: int
-    shipmentDate: str
-    shipmentTime: str
-    boxes: list[SetOrderStatusResponseResultOrderDeliveryShipmentBox]
+    id: Optional[int] = None
+    shipmentDate: Optional[str] = None
+    shipmentTime: Optional[str] = None
+    boxes: Optional[list[SetOrderStatusResponseResultOrderDeliveryShipmentBox]] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDeliveryRegionParentParent:
-    id: int
-    name: str
-    type: str
+    id: Optional[int] = None
+    name: Optional[str] = None
+    type: Optional[str] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDeliveryRegionParent:
-    id: int
-    name: str
-    type: str
-    parent: SetOrderStatusResponseResultOrderDeliveryRegionParentParent
+    id: Optional[int] = None
+    name: Optional[str] = None
+    type: Optional[str] = None
+    parent: Optional[SetOrderStatusResponseResultOrderDeliveryRegionParentParent] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDeliveryRegion:
-    id: int
-    name: str
-    type: str
-    parent: SetOrderStatusResponseResultOrderDeliveryRegionParent
+    id: Optional[int] = None
+    name: Optional[str] = None
+    type: Optional[str] = None
+    parent: Optional[SetOrderStatusResponseResultOrderDeliveryRegionParent] = None
 
 
 
@@ -93,50 +93,49 @@ class SetOrderStatusResponseResultOrderDeliveryDates:
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResultOrderDelivery:
-    deliveryPartnerType: str
-    id: str
-    outletCode: str
-    price: float
-    serviceName: str
-    type: str
-    vat: str
-    outletId: int
-    dates: SetOrderStatusResponseResultOrderDeliveryDates
-    region: SetOrderStatusResponseResultOrderDeliveryRegion
-    shipments: list[SetOrderStatusResponseResultOrderDeliveryShipment]
-
-
-@dataclass_json(undefined=Undefined.EXCLUDE)
-@dataclass
-class SetOrderStatusResponseResultOrder:
-    cancelRequested: bool
-    creationDate: str #TODO
-    currency: str
-    fake: bool
-    id: int
-    itemsTotal: float
-    paymentType: str
-    paymentMethod: str
-    status: str
-    substatus: str
-    taxSystem: str
-    total: float
-    subsidyTotal: float
-    delivery: SetOrderStatusResponseResultOrderDelivery
-    notes: str
+    deliveryPartnerType: Optional[str] = None
+    id: Optional[str] = None
+    outletCode: Optional[str] = None
+    price: Optional[float] = None
+    serviceName: Optional[str] = None
+    type: Optional[str] = None
+    vat: Optional[str] = None
+    outletId: Optional[int] = None
+    dates: Optional[SetOrderStatusResponseResultOrderDeliveryDates] = None
+    region: Optional[SetOrderStatusResponseResultOrderDeliveryRegion] = None
+    shipments: Optional[list[SetOrderStatusResponseResultOrderDeliveryShipment]] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class SetOrderStatusResponseResult:
-    order: SetOrderStatusResponseResultOrder
+    id: Optional[int] = None
+    status: Optional[str] = None
+    substatus: Optional[str] = None
+    creationDate: Optional[str] = None #TODO
+    currency: Optional[str] = None
+    cancelRequested: Optional[bool] = None
+    itemsTotal: Optional[float] = None
+    total: Optional[float] = None
+    buyerTotal: Optional[int] = None
+    buyerItemsTotal: Optional[int] = None
+    buyerTotalBeforeDiscount: Optional[int] = None
+    buyerItemsTotalBeforeDiscount: Optional[int] = None
+    deliveryTotal: Optional[int] = None
+    subsidyTotal: Optional[float] = None
+    totalWithSubsidy: Optional[float] = None
+    paymentType: Optional[str] = None
+    paymentMethod: Optional[str] = None
+    fake: Optional[bool] = None
+    feeUE: Optional[int] = None
+    delivery: Optional[SetOrderStatusResponseResultOrderDelivery] = None
+    notes: Optional[str] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
-class SetOrderStatusResponseWrapper:
-    status: Optional[str]
-    result: Optional[SetOrderStatusResponseResult]
+class SetOrderStatusResponseResultWrapper:
+    order: Optional[SetOrderStatusResponseResult] = None
 
 
 def set_order_status(
@@ -144,12 +143,11 @@ def set_order_status(
     campaign_id: str,
     order_id: int,
     data: SetStatusRequest,
-) -> SetOrderStatusResponseWrapper:
-    print(f"/campaigns/{campaign_id}/orders/{order_id}/status")
+) -> SetOrderStatusResponseResultWrapper:
     return request_api.request_api_json(
         "PUT",
         f"/campaigns/{campaign_id}/orders/{order_id}/status",
         credentials,
         data,
-        response_cls=SetOrderStatusResponseWrapper,
+        response_cls=SetOrderStatusResponseResultWrapper,
     )
